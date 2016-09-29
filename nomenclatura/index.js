71737bdf8752654a742e4c23de523f2647b3839f
root <root@localhost.localdomain>
2016-09-29 04:46:41 -0300

    nomenclaturaaa de compostos organicos

---
+++ nomenclatura/index.js
@@ -0,0 +1,69 @@
+const sufixos = [
+	{
+    grupoFuncional: 'Hidrocarboneto',
+    sufixo: 'o',
+  },
+  {
+    grupoFuncional: 'Alcool',
+    sufixo: 'ol',
+  },
+ {
+    grupoFuncional: 'Aldeído',
+    sufixo: 'al',
+  },
+  {
+    grupoFuncional: 'Cetona',
+    sufixo: 'ona',
+  },
+  {
+    grupoFuncional: 'Amina',
+    sufixo: 'amina',
+  },
+  {
+    grupoFuncional: 'Amida',
+    sufixo: 'amida',
+  }
+   
+]
+
+const prefixos = [
+  {
+    descricao: 'Cadeia que contém apenas um carbono',
+    prefixo: 'met'
+  },
+  {
+    descricao: 'Cadeia que contém dois carbonos',
+    prefixo: 'et'
+  },
+  {
+    descricao: 'Cadeia que contém três carbonos',
+    prefixo: 'prop'
+  }
+
+]
+
+const findSufixo = (molecula) => {
+  let grupo = ''
+  sufixos.forEach((el, i) => {
+    if (molecula.endsWith(el.sufixo)) grupo = el.grupoFuncional
+  })
+  return grupo
+}
+
+const findPrefixo = (molecula) => {
+  let descricao = ''
+  prefixos.forEach((el, i) => {
+    if (molecula.startsWith(el.prefixo)) descricao = el.descricao
+  })
+  return descricao
+}
+
+console.log('etanol: ', findSufixo('etanol'))
+console.log('metanal: ', findSufixo('metanal'))
+console.log('propanamina: ', findSufixo('propanamina'))
+console.log('butanamida: ', findSufixo('butanamida'))
+console.log('metano: ', findSufixo('metano'))
+
+console.log('metano: ', findPrefixo('metano'))
+console.log('etano: ', findPrefixo('etano'))
+console.log('propano: ', findPrefixo('propano'))
---
+++ nomenclatura/readme.md
@@ -0,0 +1,33 @@
+
+
+
+# QUÍMICA ORGANICA - AULA 1
+
+
+
+Postulados de Kekulé: 
+
+Carbono forma cadeias, é tetravalente com as 4 valências iguais
+Sempre, sempre, sempre o átomo de Hidrogênio estará ligado aos átomos de Carbono, por esse fato, muitas vezes ele não é representado na cadeia, mas ele está lá. 
+
+Grupos Funcionais:
+
+
+
+Regras básicas de nomenclatura (passos): 
+
+a) Destacar a cadeia carbônica mais longa, caso encontre duas cadeias com número iguais de carbonos, opte pela cadeia que contenha ramificações ou ligações duplas e triplas;
+Numerar os átomos de carbono presentes na cadeia mais longa;
+b) Contar o número de carbonos destacados
+	b1)Esta informação será o infixo do nome da cadeia;
+c) Destacar todos as ramificações e ligações duplas e triplas presentes na cadeia;
+	c1)Listar as ramicações por ordem alfabética
+	c2)Listar as duplas ou triplas ligações
+d)Ordem de importância para numerar os carbonos quando se tem ligações e ramicações:
+	d2)Ligações triplas > ligações duplas > radicais
+
+
+Nomenclatura hidrocarbonetos
+
+
+
